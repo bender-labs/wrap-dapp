b4de9fc2e20daef313f01ecd395a676d2ad85828
Arnaud LEMAIRE <arnaud@lgo.exchange>
2021-01-18 22:17:31 +0100

    doc: display token list & icon

--- src/components/swap/SwapCard.tsx
+++ src/components/swap/SwapCard.tsx
@@ -1,15 +1,28 @@
-import React from "react"
-import {Button, Card, CardActions, CardContent, CardHeader, makeStyles} from "@material-ui/core";
+import React, {useState} from "react"
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  FormControl, FormHelperText,
+  InputLabel, ListItemIcon,
+  makeStyles, MenuItem, Select
+} from "@material-ui/core";
 import SwapHorizontalCircleIcon from '@material-ui/icons/SwapHorizontalCircle';
 import {useEthereumConfig} from "../config/ConfigContext";
 import {useWeb3React} from "@web3-react/core";
 import {Web3Provider} from "@ethersproject/providers";
 import SwapCardEmptyState from "./SwapCardEmptyState";
+import TokenIcon from "../ethereum/TokenIcon";
 type Props = {}
 
 const useStyles = makeStyles((theme) => ({
   swapContainer:{
     flex: 1
+  },
+  form: {
+    width: "100%"
   }
 }));
 
@@ -17,12 +30,14 @@ export default function SwapCard({}: Props) {
   const classes = useStyles();
   const {tokens, benderCustodianContracts} = useEthereumConfig();
   const {activate, active, account, chainId} = useWeb3React<Web3Provider>()
+  const [token, setToken] = useState();
 
   if(!active || chainId == null ){
     return <SwapCardEmptyState />;
   }
 
   const tokenList = tokens[chainId];
+  const tokenOptions = Object.entries(tokenList);
   const benderContract = tokens[chainId];
   return (
       <Card className={classes.swapContainer}>
@@ -31,7 +46,31 @@ export default function SwapCard({}: Props) {
           avatar={<SwapHorizontalCircleIcon />}
           subheader="A BenderLabs 🤖 project" />
         <CardContent >
-          H2H2H2
+          <FormControl className={classes.form}>
+            <InputLabel shrink htmlFor="token-selector">
+              Select the token you want to swap
+            </InputLabel>
+            <Select
+              value={token}
+              onChange={e => alert(e)}
+              displayEmpty
+              inputProps={{
+                name: 'token',
+                id: 'token-selector',
+              }}
+            >
+                <MenuItem value={undefined} disabled>Please select</MenuItem>
+              {tokenOptions.map(([key, {name}]) => (
+                <MenuItem value={key} key={key}>
+                  <ListItemIcon>
+                    <TokenIcon fontSize="small" token={key} />
+                  </ListItemIcon>
+                  {name} ({key})
+                </MenuItem>
+              ))}
+            </Select>
+            <FormHelperText>Only supported token are listed</FormHelperText>
+          </FormControl>
         </CardContent>
 
         <CardActions>
--- src/components/swap/SwapCardEmptyState.tsx
+++ src/components/swap/SwapCardEmptyState.tsx
@@ -1,14 +1,11 @@
 import {Card, CardContent, CardHeader, makeStyles, Typography} from "@material-ui/core";
 import SwapHorizontalCircleIcon from "@material-ui/icons/SwapHorizontalCircle";
 import React from "react";
-import {blueGrey, grey} from "@material-ui/core/colors";
+import {grey} from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
   swapContainer:{
     flex: 1,
-    backgroundColor: grey[300]
-  },
-  content: {
     backgroundColor: grey[300]
   }
 }));
@@ -21,7 +18,7 @@ export default function SwapCardEmptyState() {
       title="Swap"
       avatar={<SwapHorizontalCircleIcon/>}
       subheader="A BenderLabs 🤖 project"/>
-    <CardContent className={classes.content}>
+    <CardContent>
       <Typography variant="body1">
         Please connect your accounts before we can proceed.
       </Typography>
