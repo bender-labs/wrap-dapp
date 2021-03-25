abfcb3f298c1c39b59f6b1cf967c177f6894482c
Arnaud LEMAIRE <arnaud@lgo.exchange>
2021-03-25 12:00:14 +0100

    refactor wrap: add amount selector

--- src/screens/WrapScreen.tsx
+++ src/screens/WrapScreen.tsx
@@ -6,7 +6,7 @@ import SwapCard from "../components/wrap/SwapCard";
 import {useWeb3React} from "@web3-react/core";
 import {Web3Provider} from "@ethersproject/providers";
 import WrapEmptyStateCard from "../components/wrap/WrapEmptyStateCard";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {ConnectionStatus, useTezosContext} from "../components/tezos/TezosContext";
 import {MintCard} from "../components/wrap/MintCard";
 import {BurnCard} from "../components/unwrap/BurnCard";
@@ -15,6 +15,8 @@ import {grey} from "@material-ui/core/colors";
 import TokenSelection from "../components/wrap/TokenSelection";
 import {SupportedBlockchain} from "../features/wallet/blockchain";
 import {useConfig} from "../components/config/ConfigContext";
+import AmountToWrapInput from "../components/wrap/AmountToWrapInput";
+import BigNumber from "bignumber.js";
 
 enum TabValues {
   WRAP,
@@ -39,15 +41,27 @@ export default () => {
   const {activate: tzActivate, status: tzConnectionStatus, library: tzLibrary, account: tzAccount} = useTezosContext();
   const {fungibleTokens} = useConfig();
   const [token, setToken] = useState("");
+
+  useEffect(() => {
+
+  }, [token]);
+
+  const [amountToWrap, setAmountToWrap] = useState<BigNumber>(new BigNumber(0));
   return (
     <Paper className={classes.swapContainer}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <EthWalletConnection account={ethAccount} activate={ethActivate} active={ethActive}/>
         </Grid>
-        <Grid item xs={12}>
-          <TokenSelection token={token} onTokenSelect={setToken} tokens={fungibleTokens}
-                          blockchainTarget={SupportedBlockchain.Ethereum}/>
+        <Grid container item xs={12}>
+          <Grid item xs={6}>
+            <AmountToWrapInput balance={new BigNumber(0)} decimals={18} symbol={token} onChange={setAmountToWrap}
+                               amountToWrap={amountToWrap}/>
+          </Grid>
+          <Grid item xs={6}>
+            <TokenSelection token={token} onTokenSelect={setToken} tokens={fungibleTokens}
+                            blockchainTarget={SupportedBlockchain.Ethereum}/>
+          </Grid>
         </Grid>
         <Grid item xs={12}>
           <TezosWalletConnection account={tzAccount} activate={tzActivate} status={tzConnectionStatus}/>
