d2a385c63fedbcc86915326806851154a90903df
Charles Couillard <charles@benderlabs.io>
2021-06-10 09:46:28 +0200

    feat: add period progress to farm list

--- src/screens/farming/FarmChoice.tsx
+++ src/screens/farming/FarmChoice.tsx
@@ -1,140 +1,157 @@
-import {Box, Container, Grid, IconButton, Typography} from '@material-ui/core';
-import {createStyles, makeStyles} from '@material-ui/core/styles';
+import {
+  Box,
+  Container,
+  Grid,
+  IconButton,
+  Typography,
+} from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import React from 'react';
-import {useConfig} from '../../runtime/config/ConfigContext';
+import { useConfig } from '../../runtime/config/ConfigContext';
 import FarmList from './FarmList';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import {PaperContent} from '../../components/paper/Paper';
-import {useHistory} from 'react-router';
-import {farmStakePageRoute, paths} from '../routes';
+import { PaperContent } from '../../components/paper/Paper';
+import { useHistory } from 'react-router';
+import { farmStakePageRoute, paths } from '../routes';
 import BigNumber from 'bignumber.js';
-import logo from './Logo/7.png'
+import logo from './Logo/7.png';
 
 const useStyles = makeStyles((theme) =>
-    createStyles({
-        subtitle: {
-            color: '#000000',
-            textAlign: 'center',
-            marginBottom: '20px',
-        },
-        main: {
-            borderRadius: '10px 10px 10px 10px',
-            backgroundColor: 'white',
-            marginBottom: '17px',
-            transition: 'background-color 1s ease',
-            '&:hover': {
-                backgroundColor: '#FFD000'
-            },
-        },
-        containBox: {
-            borderRadius: '0 0 10px 10px',
-            padding: '30px',
-            backgroundColor: '#e5e5e5',
-        },
-        title: {
-            color: 'white',
-            borderBottom: '3px solid #ffd000',
-            textAlign: 'center',
-            fontSize: '30px',
-            paddingBottom: '15px',
-        },
-        titleCenter: {
-            justifyItems: 'center',
-        },
-        total: {
-            color: 'white',
-            textAlign: 'center',
-            fontSize: '16px',
-        },
-        option: {
-            fontSize: '20px'
-        },
-        sub: {
-            fontSize: '12px'
-        },
-        item: {
-
-            '&:hover': {
-                cursor: 'pointer'
-            }
-        },
-        image: {
-            height: '100%',
-            width: '100%'
-        },
-        images: {
-            '& img': {width: 60, height: 50, marginRight: 5, verticalAlign: 'middle'},
-            '& :first-child': {left: '0', position: 'relative'}
-        }
-    }));
-
+  createStyles({
+    subtitle: {
+      color: '#000000',
+      textAlign: 'center',
+      marginBottom: '20px',
+    },
+    main: {
+      borderRadius: '10px 10px 10px 10px',
+      backgroundColor: 'white',
+      marginBottom: '17px',
+      transition: 'background-color 1s ease',
+      '&:hover': {
+        backgroundColor: '#FFD000',
+      },
+    },
+    containBox: {
+      borderRadius: '0 0 10px 10px',
+      padding: '30px',
+      backgroundColor: '#e5e5e5',
+    },
+    title: {
+      color: 'white',
+      borderBottom: '3px solid #ffd000',
+      textAlign: 'center',
+      fontSize: '30px',
+      paddingBottom: '15px',
+    },
+    titleCenter: {
+      justifyItems: 'center',
+    },
+    total: {
+      color: 'white',
+      textAlign: 'center',
+      fontSize: '16px',
+    },
+    option: {
+      fontSize: '20px',
+    },
+    sub: {
+      fontSize: '12px',
+    },
+    item: {
+      '&:hover': {
+        cursor: 'pointer',
+      },
+    },
+    image: {
+      height: '100%',
+      width: '100%',
+    },
+    images: {
+      '& img': {
+        width: 60,
+        height: 50,
+        marginRight: 5,
+        verticalAlign: 'middle',
+      },
+      '& :first-child': { left: '0', position: 'relative' },
+    },
+  })
+);
 
 export default function FarmChoice() {
-    const classes = useStyles();
-    const history = useHistory();
-    const {farms} = useConfig();
+  const classes = useStyles();
+  const history = useHistory();
+  const { farms } = useConfig();
 
-    const totalStaked = farms
-        .reduce((total, farm) => {
-            const staked = new BigNumber(farm.farmTotalStaked).shiftedBy(
-                -farm.farmStakedToken.decimals
-            );
-            return total.plus(staked);
-        }, new BigNumber(0))
-        .dp(0)
-        .toString(10);
+  const totalStaked = farms
+    .reduce((total, farm) => {
+      const staked = new BigNumber(farm.farmTotalStaked).shiftedBy(
+        -farm.farmStakedToken.decimals
+      );
+      return total.plus(staked);
+    }, new BigNumber(0))
+    .dp(0)
+    .toString(10);
 
-    const StakeAllButton = () => {
-
-        const changeUri = () => {
-            history.push(paths.ALL_FARMS_STAKE);
-        }
-
-        return (
-            <PaperContent className={classes.main}>
-                <Grid container justify={'space-between'} alignItems={'center'} onClick={changeUri}
-                      className={classes.item}>
-                    <Grid item className={classes.images}>
-
-                        <img src={logo}/>
-                    </Grid>
-                    <Grid item>
-                        <Typography className={classes.option}>
-                            Stake on all farms
-                        </Typography>
-                        <Typography className={classes.sub}>
-                            Stake your $WRAP tokens on all available farms at once
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <IconButton>
-                            <ArrowForwardIcon/>
-                        </IconButton>
-                    </Grid>
-                </Grid>
-            </PaperContent>
-        );
-    }
+  const StakeAllButton = () => {
+    const changeUri = () => {
+      history.push(paths.ALL_FARMS_STAKE);
+    };
 
     return (
-        <Container maxWidth={'sm'}>
-            <Box className={classes.titleCenter} my={2}>
-                <Typography className={classes.title}>Fees farming</Typography>
-            </Box>
-            <Box className={classes.titleCenter} my={2}>
-                <Typography className={classes.total}>
-                    Total $WRAP staked : {totalStaked}
-                </Typography>
-            </Box>
-            <Box className={classes.containBox}>
-                <Typography variant={'subtitle1'} className={classes.subtitle}>Select a farm to stake, unstake or claim
-                    your fees share.</Typography>
-                <StakeAllButton/>
-                <Typography variant={'subtitle1'} className={classes.subtitle}>Or choose one farm directly</Typography>
-                <FarmList farms={farms} onProgramSelect={(farmContract) => {
-                    history.push(farmStakePageRoute(farmContract));
-                }}/>
-            </Box>
-        </Container>
+      <PaperContent className={classes.main}>
+        <Grid
+          container
+          justify={'space-between'}
+          alignItems={'center'}
+          onClick={changeUri}
+          className={classes.item}
+        >
+          <Grid item className={classes.images}>
+            <img src={logo} />
+          </Grid>
+          <Grid item>
+            <Typography className={classes.option}>
+              Stake on all farms
+            </Typography>
+            <Typography className={classes.sub}>
+              Stake your $WRAP tokens on all available farms at once
+            </Typography>
+          </Grid>
+          <Grid item>
+            <IconButton>
+              <ArrowForwardIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </PaperContent>
     );
+  };
+
+  return (
+    <Container maxWidth={'sm'}>
+      <Box className={classes.titleCenter} my={2}>
+        <Typography className={classes.title}>Fees farming</Typography>
+      </Box>
+      <Box className={classes.titleCenter} my={2}>
+        <Typography className={classes.total}>
+          Total $WRAP staked : {totalStaked}
+        </Typography>
+      </Box>
+      <Box className={classes.containBox}>
+        <Typography variant={'subtitle1'} className={classes.subtitle}>
+          Select a farm to stake, unstake or claim your fees share.
+        </Typography>
+        {/*<StakeAllButton/>
+                <Typography variant={'subtitle1'} className={classes.subtitle}>Or choose one farm directly</Typography>*/}
+        <FarmList
+          farms={farms}
+          onProgramSelect={(farmContract) => {
+            history.push(farmStakePageRoute(farmContract));
+          }}
+        />
+      </Box>
+    </Container>
+  );
 }
--- src/screens/farming/FarmList.tsx
+++ src/screens/farming/FarmList.tsx
@@ -1,112 +1,162 @@
-import {FarmConfig} from '../../config';
-import {PaperContent} from '../../components/paper/Paper';
-import {Grid, IconButton, Typography,} from '@material-ui/core';
-import {createStyles, makeStyles} from '@material-ui/core/styles';
+import { FarmConfig } from '../../config';
+import { PaperContent } from '../../components/paper/Paper';
+import { Grid, IconButton, Typography } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import TezosTokenIcon from '../../components/icons/TezosTokenIcon';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import React from 'react';
-import BigNumber from "bignumber.js";
-import {DateTime} from "luxon";
+import BigNumber from 'bignumber.js';
+import { DateTime } from 'luxon';
 
 export type FarmListProps = {
-    farms: FarmConfig[];
-    onProgramSelect: (farm: string) => void;
+  farms: FarmConfig[];
+  onProgramSelect: (farm: string) => void;
 };
 
-const useStyle = makeStyles(() => createStyles({
+const useStyle = makeStyles(() =>
+  createStyles({
     main: {
-        borderRadius: '10px 10px 10px 10px',
-        backgroundColor: 'white',
-        transition: 'background-color 1s ease',
-        '&:hover': {
-            backgroundColor: '#FFD000'
-        },
+      borderRadius: '10px 10px 10px 10px',
+      backgroundColor: 'white',
+      transition: 'background-color 1s ease',
+      '&:hover': {
+        backgroundColor: '#FFD000',
+      },
     },
     option: {
-        fontSize: '20px'
+      fontSize: '20px',
     },
     rewards: {
-        fontSize: '12px'
+      fontSize: '12px',
+
+      '& > span': {
+        fontWeight: 900,
+      },
     },
     item: {
-        '&:hover': {
-            cursor: 'pointer'
-        }
+      '&:hover': {
+        cursor: 'pointer',
+      },
     },
     leftGridItem: {
-        flexGrow: 1,
-        paddingLeft: "20px",
-        textAlign: "left"
+      flexGrow: 1,
+      paddingLeft: '20px',
+      textAlign: 'left',
     },
     images: {
-        '& img': {width: 60, height: 60, marginRight: 5, verticalAlign: 'middle'},
-        '& :first-child': {left: '0', position: 'relative'}
-    }
-}));
+      '& img': {
+        width: 60,
+        height: 60,
+        marginRight: 5,
+        verticalAlign: 'middle',
+      },
+      '& :first-child': { left: '0', position: 'relative' },
+    },
+  })
+);
 
 function Rewards({
-                     farmConfig,
-                     className
-                 }: { farmConfig: FarmConfig, className: string }) {
-    if (farmConfig.rewards) {
-        const currentTotalRewards = new BigNumber(farmConfig.rewards.totalRewards).shiftedBy(-farmConfig.rewardTokenDecimals).dp(8).toString(10);
-        const currentStakedTotens = new BigNumber(farmConfig.farmTotalStaked).shiftedBy(-farmConfig.farmStakedToken.decimals).dp(8).toString(10);
-        const nextRewardTime = DateTime.fromMillis(new Date(farmConfig.rewards.startTimestamp).getTime()).plus({"minutes": parseInt(farmConfig.rewards.duration)});
-
-        const nextRewardLabel = nextRewardTime.toMillis() < new Date().getTime() ?
-            "Awaiting new rewards period" :
-            `New rewards will be added ${nextRewardTime.toRelative({locale: 'en'})}`;
-
-        return (
-            <>
-                <Typography className={className}>Current
-                    available {farmConfig.rewardTokenSymbol} rewards: {currentTotalRewards}</Typography>
-                <Typography className={className}>Current
-                    staked {farmConfig.farmStakedToken.symbol} tokens: {currentStakedTotens}</Typography>
-                <Typography className={className}>{nextRewardLabel}</Typography>
-            </>
-        );
-    }
-    return (<></>);
-}
-
-function FarmSelector({farmConfig, onClick}: {
-    farmConfig: FarmConfig;
-    onClick: () => void;
+  farmConfig,
+  className,
+}: {
+  farmConfig: FarmConfig;
+  className: string;
 }) {
-    const classes = useStyle();
+  if (farmConfig.rewards) {
+    const currentTotalRewards = new BigNumber(farmConfig.rewards.totalRewards)
+      .shiftedBy(-farmConfig.rewardTokenDecimals)
+      .dp(8)
+      .toString(10);
+    const currentStakedTotens = new BigNumber(farmConfig.farmTotalStaked)
+      .shiftedBy(-farmConfig.farmStakedToken.decimals)
+      .dp(8)
+      .toString(10);
+    const nextRewardTime = DateTime.fromISO(
+      farmConfig.rewards.startTimestamp
+    ).plus({ minutes: parseInt(farmConfig.rewards.duration) });
+    const currentPeriodProgress =
+      (1 -
+        nextRewardTime.diff(DateTime.local().toUTC(), 'minutes').minutes /
+          parseInt(farmConfig.rewards.duration)) *
+      100;
+
+    const nextRewardLabel =
+      nextRewardTime.toMillis() < DateTime.local().toUTC().toMillis()
+        ? 'Awaiting new rewards period'
+        : `New rewards will be added ${nextRewardTime.toRelative({
+            locale: 'en',
+          })}`;
 
     return (
-        <PaperContent className={classes.main}>
-            <Grid container justify={'space-between'} alignItems={'center'} onClick={onClick} className={classes.item}>
-                <Grid item className={classes.images}>
-                    <TezosTokenIcon url={farmConfig.rewardTokenThumbnailUri ?? "ipfs://"}/>
-                </Grid>
-                <Grid item className={classes.leftGridItem}>
-                    <Typography className={classes.option}>
-                        {farmConfig.rewardTokenName} farm
-                    </Typography>
-                    <Rewards farmConfig={farmConfig} className={classes.rewards}/>
-                </Grid>
-                <Grid item>
-                    <IconButton>
-                        <ArrowForwardIcon/>
-                    </IconButton>
-                </Grid>
-            </Grid>
-        </PaperContent>
+      <>
+        <Typography className={className}>
+          Current available {farmConfig.rewardTokenSymbol} rewards:{' '}
+          <span>{currentTotalRewards}</span>
+        </Typography>
+        <Typography className={className}>
+          Current staked {farmConfig.farmStakedToken.symbol} tokens:{' '}
+          <span>{currentStakedTotens}</span>
+        </Typography>
+        <Typography className={className}>
+          Period progress: <span>{currentPeriodProgress.toFixed(1)}%</span>
+        </Typography>
+        <Typography className={className}>{nextRewardLabel}</Typography>
+      </>
     );
+  }
+  return <></>;
 }
 
-export default function FarmList({farms, onProgramSelect}: FarmListProps) {
-    return (
-        <Grid container spacing={2} direction={'column'}>
-            {farms.map((farmConfig) => (
-                <Grid item key={farmConfig.farmContractAddress}>
-                    <FarmSelector farmConfig={farmConfig}
-                                  onClick={() => onProgramSelect(farmConfig.farmContractAddress)}/>
-                </Grid>
-            ))}
+function FarmSelector({
+  farmConfig,
+  onClick,
+}: {
+  farmConfig: FarmConfig;
+  onClick: () => void;
+}) {
+  const classes = useStyle();
+
+  return (
+    <PaperContent className={classes.main}>
+      <Grid
+        container
+        justify={'space-between'}
+        alignItems={'center'}
+        onClick={onClick}
+        className={classes.item}
+      >
+        <Grid item className={classes.images}>
+          <TezosTokenIcon
+            url={farmConfig.rewardTokenThumbnailUri ?? 'ipfs://'}
+          />
         </Grid>
-    );
-};
+        <Grid item className={classes.leftGridItem}>
+          <Typography className={classes.option}>
+            {farmConfig.rewardTokenName} farm
+          </Typography>
+          <Rewards farmConfig={farmConfig} className={classes.rewards} />
+        </Grid>
+        <Grid item>
+          <IconButton>
+            <ArrowForwardIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </PaperContent>
+  );
+}
+
+export default function FarmList({ farms, onProgramSelect }: FarmListProps) {
+  return (
+    <Grid container spacing={2} direction={'column'}>
+      {farms.map((farmConfig) => (
+        <Grid item key={farmConfig.farmContractAddress}>
+          <FarmSelector
+            farmConfig={farmConfig}
+            onClick={() => onProgramSelect(farmConfig.farmContractAddress)}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
